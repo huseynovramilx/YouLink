4b9a62baf63db41f147eb1ac5256940c78311eb7
Ramil Huseynov <huseynovramilism@gmail.com>
2019-07-09 05:06:16 +0400

    Fixes

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LinkShortener.Models;
-using LinkShortener.Models;
 using Microsoft.Extensions.Options;
 
 namespace LinkShortener.Areas.Admin.Controllers
